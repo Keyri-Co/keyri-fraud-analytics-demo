7e8056461419aba3d8f6649546e665c76fcb28ad
Zain Azeem <zain.azeem@gmail.com>
2023-06-07 11:27:32 -0400

    Targeting staging API

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -47,7 +47,7 @@ const AuthForm = () => {
     let device = new Device({
       apiKey: process.env.NEXT_PUBLIC_FINGERPRINT_API_KEY,
       serviceEncryptionKey: process.env.NEXT_PUBLIC_RP_ENCRYPTION_PUBLIC_KEY,
-      environment: 'development',
+      environment: 'staging',
     });
     await device.load();
     const encryptedSignupEvent = await device.generateEvent({
@@ -61,7 +61,12 @@ const AuthForm = () => {
     const res = await fetch('/api/signup', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, password, publicKey, encryptedSignupEventString }),
+      body: JSON.stringify({
+        username,
+        password,
+        publicKey,
+        encryptedSignupEventString,
+      }),
     });
 
     const response = await res.json();
@@ -105,7 +110,7 @@ const AuthForm = () => {
     let device = new Device({
       apiKey: process.env.NEXT_PUBLIC_FINGERPRINT_API_KEY,
       serviceEncryptionKey: process.env.NEXT_PUBLIC_RP_ENCRYPTION_PUBLIC_KEY,
-      environment: 'development',
+      environment: 'staging',
     });
     await device.load();
     const encryptedLoginEvent = await device.generateEvent({
@@ -119,7 +124,12 @@ const AuthForm = () => {
     const res = await fetch('/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, password, publicKey, encryptedLoginEventString }),
+      body: JSON.stringify({
+        username,
+        password,
+        publicKey,
+        encryptedLoginEventString,
+      }),
     });
     const response = await res.json();
     const riskResponse = JSON.parse(response.riskResponse);
@@ -165,7 +175,7 @@ const AuthForm = () => {
     const device = new Device({
       apiKey: process.env.NEXT_PUBLIC_FINGERPRINT_API_KEY,
       serviceEncryptionKey: process.env.NEXT_PUBLIC_RP_ENCRYPTION_PUBLIC_KEY,
-      environment: 'development',
+      environment: 'staging',
     });
     await device.load();
     const encryptedRiskEvent = await device.generateEvent({
@@ -190,39 +200,44 @@ const AuthForm = () => {
   };
 
   return (
-    <div className='w-full'>
-      <div className='flex flex-col mb-4'>
-        <h1 className='w-full max-w-sm mx-auto text-2xl font-bold'>{isLogin ? 'Log in' : 'Register'}</h1>
+    <div className="w-full">
+      <div className="flex flex-col mb-4">
+        <h1 className="w-full max-w-sm mx-auto text-2xl font-bold">
+          {isLogin ? 'Log in' : 'Register'}
+        </h1>
       </div>
-      <form className='w-full max-w-sm mx-auto' onSubmit={isLogin ? handleLogin : handleSignup}>
-        <div className='flex flex-col mb-4'>
+      <form
+        className="w-full max-w-sm mx-auto"
+        onSubmit={isLogin ? handleLogin : handleSignup}
+      >
+        <div className="flex flex-col mb-4">
           <input
-            className='py-2 px-3 border rounded placeholder-gray-500 focus:outline-none text-gray-800'
-            type='text'
-            placeholder='Username'
+            className="py-2 px-3 border rounded placeholder-gray-500 focus:outline-none text-gray-800"
+            type="text"
+            placeholder="Username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             required
           />
         </div>
-        <div className='flex flex-col mb-4'>
+        <div className="flex flex-col mb-4">
           <input
-            className='py-2 px-3 border rounded placeholder-gray-500 focus:outline-none text-gray-800'
-            type='password'
-            placeholder='Password'
+            className="py-2 px-3 border rounded placeholder-gray-500 focus:outline-none text-gray-800"
+            type="password"
+            placeholder="Password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
           />
         </div>
-        <div className='flex flex-col mb-4'>
+        <div className="flex flex-col mb-4">
           <button
-            className='bg-[#934D91] hover:bg-[#A0549D] text-white font-bold py-2 px-4 rounded focus:outline-none flex items-center justify-center'
-            type='submit'
+            className="bg-[#934D91] hover:bg-[#A0549D] text-white font-bold py-2 px-4 rounded focus:outline-none flex items-center justify-center"
+            type="submit"
             disabled={loading}
           >
             {loading ? (
-              <div className='animate-spin w-4 h-4 border-t-2 border-white rounded-full' />
+              <div className="animate-spin w-4 h-4 border-t-2 border-white rounded-full" />
             ) : isLogin ? (
               'Log in'
             ) : (
@@ -231,31 +246,38 @@ const AuthForm = () => {
           </button>
         </div>
       </form>
-      <div className='flex flex-col mb-4'>
-        {authError && <p className='text-red-500 w-full max-w-sm mx-auto'>{authError}</p>}
-        <p className='w-full max-w-sm mx-auto hover:text-[#A0549D] hover:cursor-pointer' onClick={toggleAuthMode}>
-          {isLogin ? "Don't have an account? Register" : 'Already have an account? Log in'}
+      <div className="flex flex-col mb-4">
+        {authError && (
+          <p className="text-red-500 w-full max-w-sm mx-auto">{authError}</p>
+        )}
+        <p
+          className="w-full max-w-sm mx-auto hover:text-[#A0549D] hover:cursor-pointer"
+          onClick={toggleAuthMode}
+        >
+          {isLogin
+            ? "Don't have an account? Register"
+            : 'Already have an account? Log in'}
         </p>
       </div>
 
-      <div className='flex flex-col mb-4 w-full max-w-sm mx-auto'>
-        <h3 className='text-lg font-semibold mb-4 border-t border-gray-600 py-4'>
+      <div className="flex flex-col mb-4 w-full max-w-sm mx-auto">
+        <h3 className="text-lg font-semibold mb-4 border-t border-gray-600 py-4">
           Or authenticate with JWT from other session
         </h3>
         <input
-          className='py-2 px-3 border rounded placeholder-gray-500 focus:outline-none text-gray-800 mb-4'
-          type='text'
-          placeholder='Paste your JWT here'
+          className="py-2 px-3 border rounded placeholder-gray-500 focus:outline-none text-gray-800 mb-4"
+          type="text"
+          placeholder="Paste your JWT here"
           value={jwtInput}
           onChange={(e) => setJwtInput(e.target.value)}
         />
         <button
-          className='bg-indigo-800 border border-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none flex items-center justify-center'
+          className="bg-indigo-800 border border-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none flex items-center justify-center"
           onClick={handleJwtAuth}
           disabled={loading || !jwtInput}
         >
           {loading ? (
-            <div className='animate-spin w-4 h-4 border-t-2 border-white rounded-full' />
+            <div className="animate-spin w-4 h-4 border-t-2 border-white rounded-full" />
           ) : (
             'Authenticate with JWT'
           )}
